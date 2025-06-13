b73481959d42ca40e543dd8f7e513884cb7931ad
saturn-ss <shopify.saturn@outlook.com>
2025-06-13 12:54:50 -0400

    implement attribute change handling and update UI for checkout extension

--- extensions/checkout-ui/src/Checkout.jsx
+++ extensions/checkout-ui/src/Checkout.jsx
@@ -8,7 +8,6 @@ import {
   useApplyAttributeChange,
   useInstructions,
   useTranslate,
-  Divider,
 } from "@shopify/ui-extensions-react/checkout";
 
 // 1. Choose an extension target
@@ -17,10 +16,44 @@ export default reactExtension("purchase.checkout.block.render", () => (
 ));
 
 function Extension() {
+  const translate = useTranslate();
+  const { extension } = useApi();
+  const instructions = useInstructions();
+  const applyAttributeChange = useApplyAttributeChange();
+
+
+  // 2. Check instructions for feature availability, see https://shopify.dev/docs/api/checkout-ui-extensions/apis/cart-instructions for details
+  if (!instructions.attributes.canUpdateAttributes) {
+    // For checkouts such as draft order invoices, cart attributes may not be allowed
+    // Consider rendering a fallback UI or nothing at all, if the feature is unavailable
+    return (
+      <Banner title="checkout-ui" status="warning">
+        {translate("attributeChangesAreNotSupported")}
+      </Banner>
+    );
+  }
+
   // 3. Render a UI
   return (
-    <BlockStack border={"none"} padding={"tight"}>
-      <Divider />
+    <BlockStack border={"dotted"} padding={"tight"}>
+      <Banner title="checkout-ui">
+        {translate("welcome", {
+          target: <Text emphasis="italic">{extension.target}</Text>,
+        })}
+      </Banner>
+      <Checkbox onChange={onCheckboxChange}>
+        {translate("iWouldLikeAFreeGiftWithMyOrder")}
+      </Checkbox>
     </BlockStack>
   );
+
+  async function onCheckboxChange(isChecked) {
+    // 4. Call the API to modify checkout
+    const result = await applyAttributeChange({
+      key: "requestedFreeGift",
+      type: "updateAttribute",
+      value: isChecked ? "yes" : "no",
+    });
+    console.log("applyAttributeChange result", result);
+  }
 }
